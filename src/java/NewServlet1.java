bca561ba406a20c93253b0fb6425c151d1dde430
murugeswarii <murugeswari.sraj@gmail.com>
2018-03-31 16:36:35 +0530

    First commit

---
+++ src/java/NewServlet1.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Kala Pandi
+ */
+@WebServlet(urlPatterns = {"/NewServlet1"})
+public class NewServlet1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewServlet1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+                        try{
+              Class.forName("org.apache.derby.jdbc.ClientDriver");
+               Connection con=DriverManager.getConnection("jdbc:derby://localhost:1527/newplacement","kala","kala");
+                     
+          PreparedStatement st=con.prepareStatement("select * from student_basic_details");
+          
+            ResultSet rs=st.executeQuery();
+            while(rs.next()){
+                out.println("<table><tr><td>"+rs.getString("name")+"</td><td>"+rs.getInt("rollno")+"</td><td>"+rs.getString("department")+"</td><td>"+rs.getString("gender")+"</td><td>"+rs.getString("batch")+"</td><td>"+rs.getString("d_o_b")+"</td><td>"+rs.getString("blood_group")+"</td><td>"+rs.getString("email_id")+"</td><td>"+rs.getString("phone_no")+"</td><td>"+rs.getString("register_number")+"</td></tr><table>");
+            }
+            
+           }catch(ClassNotFoundException| SQLException e){out.println(e);
+            }
+
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/adminlogin.jsp
@@ -0,0 +1,37 @@
+<%-- 
+    Document   : adminlogin
+    Created on : Sep 27, 2015, 7:25:03 PM
+    Author     : Kala Pandi
+--%>
+<%@page import="java.io.IOException"%>
+<%@page import="java.io.PrintWriter"%>
+<%@page import="javax.servlet.ServletException"%>
+<%@page import="javax.servlet.annotation.WebServlet"%>
+<%@page import="javax.servlet.http.HttpServlet"%>
+<%@page import="javax.servlet.http.HttpServletRequest"%>
+<%@page import="javax.servlet.http.HttpServletResponse"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Admin login</title>
+        <link rel="stylesheet" href="stylesheet.css">
+    </head>
+      <body>
+          <header></header>
+<div class="page">
+<center>
+    STUDENT INFORMATION<br/>
+    <a href="NewServlet1">Students basic details</a><br/>
+     <a href="Friends">Friends </a><br/>
+      <a href="Hobbies">Hobbies</a><br/>
+       <a href="Marks">marks</a><br/>
+        <a href="Talentest">Talent test</a><br/>
+        <a href="Eligible">Eligible students</a>
+        
+      </center>
+
+
+    </body>
+</html>
